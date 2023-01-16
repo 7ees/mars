4e42c12b66609fa2f22e0d52e6a74bc07a52a3f6
Ircagea <jsrido92@gmail.com>
2023-01-16 09:20:50 +0700

    First Commit

--- start.sh
+++ start.sh
@@ -1,2 +1,18 @@
-MONGODB_URI="mongodb+srv://jubillee:Ridojs123@cluster0.do11dfg.mongodb.net/?retryWrites=true&w=majority"
-DB_NAME="dbjees"
+set -eu
+
+export PYTHONUNBUFFERED=true
+
+VIRTUALENV=.data/venv
+
+if [ ! -d $VIRTUALENV ]; then
+  python3 -m venv $VIRTUALENV
+fi
+
+if [ ! -f $VIRTUALENV/bin/pip ]; then
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
+fi
+
+$VIRTUALENV/bin/pip install -r requirements.txt
+
+$VIRTUALENV/bin/python3 app.py
+Footer
